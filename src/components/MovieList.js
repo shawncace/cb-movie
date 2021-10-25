f84c608d7af2d45ddada15cecad8603f1c0dc28a
shawn <shawncaceres10@gmail.com>
2021-10-25 07:08:23 -0500

    app init

---
+++ src/components/MovieList.js
@@ -0,0 +1,17 @@
+
+
+const MovieList = (props) => {
+  return (  
+    <>
+      {props.movies.map((movie,index)=>(
+        <div>
+          <img src={movie.Poster}        
+               alt='movie img'>
+          </img>
+        </div>
+      ))}    
+    </>
+  );
+}
+ 
+export default MovieList;
