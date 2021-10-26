3531c5918bbcba59d2f395991609a485d1a04515
shawn <shawncaceres10@gmail.com>
2021-10-26 11:59:08 -0500

    working api

--- src/App.js
+++ src/App.js
@@ -5,54 +5,27 @@ import MovieList from "./components/MovieList";
 
 
 function App() {
-  const [movies, setMovies]= useState([
-    {
-      "Title": "Starwars: Goretech",
-      "Year": "2018",
-      "imdbID": "tt9336300",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNTI5OTBhMGYtNTZlNS00MjMzLTk5NTEtZDZkODM5YjYzYmE5XkEyXkFqcGdeQXVyMzU0OTU0MzY@._V1_SX300.jpg"
-  },
-  {
-      "Title": "MeUndies x StarWars: The Dark Side",
-      "Year": "2018",
-      "imdbID": "tt9414858",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMzg3ZDcwNDQtOTVlMC00ZjE0LThiYjktY2U2YTVjODU5MWY5XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
-  },
-  {
-      "Title": "MeUndies x StarWars: The Force",
-      "Year": "2018",
-      "imdbID": "tt9414918",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BZTdjYjM1YzgtYmViMS00ODFjLTgyM2EtMjRkNTFiNDEyMTU2XkEyXkFqcGdeQXVyNjg3MDM0MzE@._V1_SX300.jpg"
-  },
-  {
-      "Title": "40 Years: StarWars & ILM",
-      "Year": "2016",
-      "imdbID": "tt5320598",
-      "Type": "movie",
-      "Poster": "N/A"
-  },
-  {
-      "Title": "The StarWars Adventures",
-      "Year": "2016–",
-      "imdbID": "tt5814352",
-      "Type": "series",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNWNkOGIyN2ItNGEwYS00ZTM2LWIyMzgtNjUxMzJmMjViZTE4XkEyXkFqcGdeQXVyMzM4MTY3ODg@._V1_SX300.jpg"
-  },
-  {
-      "Title": "The StarWars Adventures: Project Blackwing",
-      "Year": "2016–",
-      "imdbID": "tt5814452",
-      "Type": "series",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMmQyM2M3ZjItYjhiMy00YWM4LWFmZGMtNTI1NTYwYjZlOGJhXkEyXkFqcGdeQXVyMzM4MTY3ODg@._V1_SX300.jpg"
+  const [movies, setMovies]= useState([])
+
+  const getMovieRequest = async () =>{
+    const url = 'https://www.omdbapi.com/?s=star wars&apikey=3fbecb5a'
+  
+    const response = await fetch(url)
+    const responseJson = await response.json()
+    
+    console.log(responseJson)
+    setMovies(responseJson.Search)
   }
-  ])
+
+  useEffect(()=>{
+    getMovieRequest()
+  }, [])
 
   return (
-    <div>
-      <MovieList movies={movies}/>
+    <div className='project-container'>
+      <div className='movie-container'>
+        <MovieList movies={movies}/>
+      </div>
     </div>
   );
 }
